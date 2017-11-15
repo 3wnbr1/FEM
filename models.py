37c4ccfe267da9f2b6d646c320dec48893f9905e
ewen <ewen.brun@icloud.com>
2017-11-15 17:15:32 +0100

    added polys noyel

--- models.py
+++ models.py
@@ -108,7 +108,7 @@ class PoutreEnFlexion(Model):
     def solve(self):
         self._K1 = self.K().remove_null(0).remove_null(1)
         self._F = [0]*(self._K1.shape[0])
-        self._F[-2] = -100
+        self._F[-2] = -10
         self._F[-1] = 0
         self._U = nl.solve(self._K1, self._F)
 
@@ -117,8 +117,8 @@ class PoutreEnFlexion(Model):
         x = np.cumsum(self._lenght/self._elements*np.cos(self._U[1::2]))
         plt.plot(x, self._U[::2], label="FEM")
 
-        xx = np.arange(0, 1001)
-        plt.plot(xx, self._F[-2]*(xx)**3/(3*self._I*self.material.E), label="Deformee")
+        # xx = np.arange(0, 1001)
+        # plt.plot(xx, self._F[-2]*(xx)**3/(3*self._I*self.material.E), label=r"Flèche RDM : $\frac{F.L^{3}}{3.I_{gz}.E}$")
 
         plt.plot([0, self._lenght], [0, 0], label="Situation initiale")
         plt.title("FEM - Etude d'une poutre en flexion")
