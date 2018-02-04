195e8e3a649a9edd231a68b990fe9b3eb79a6241
ewen <ewen.brun@icloud.com>
2018-02-04 23:51:01 +0100

    Working graph

--- mplwidget.py
+++ mplwidget.py
@@ -2,10 +2,17 @@
 
 
 import numpy as np
-from cm import colorline
 from PyQt5 import QtWidgets
 from matplotlib.figure import Figure
+from matplotlib.collections import LineCollection
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+
+
+def make_segments(x, y):
+    """Make segments."""
+    points = np.array([x, y]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+    return segments
 
 
 class MplCanvas(FigureCanvasQTAgg):
@@ -14,7 +21,7 @@ class MplCanvas(FigureCanvasQTAgg):
     def __init__(self):
         """Init."""
         self.fig = Figure()
-        self.ax1 = self.fig.add_subplot(111)
+        self.ax = self.fig.add_subplot(111)
         FigureCanvasQTAgg.__init__(self, self.fig)
         FigureCanvasQTAgg.setSizePolicy(
             self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
@@ -22,13 +29,21 @@ class MplCanvas(FigureCanvasQTAgg):
 
     def graph(self, model):
         """Plot deformée."""
-        self.ax1.cla()
-        self.ax1.set_title('Deformée')
-        lc = colorline(model.deformee[0], np.sin(model.deformee[0]), cmap='jet')
+        self.ax.cla()
+        self.ax.set_title('Deformée')
+        lc = self.colorline(model.deformee[0], np.sin(model.deformee[0]), np.sin(model.deformee[0]))
         self.fig.colorbar(lc)
-        self.ax1.plot(model.initial[0], model.initial[1], label="Inital")
-        self.ax1.legend()
+        self.ax.plot(model.initial[0], model.initial[1], label="Inital")
+        self.ax.legend()
         self.draw()
+
+    def colorline(self, x, y, z):
+        """Plot a colored line with coordinates x and y."""
+        z = np.asarray(z)
+        segments = make_segments(x, y)
+        lc = LineCollection(segments, array=z, cmap='jet', linewidth=3, alpha=1)
+        self.ax.add_collection(lc)
+        return lc
 
 
 class MplWidget(QtWidgets.QWidget):
