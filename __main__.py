01e3e57f1480019f991d39d36123037722015bc8
ewen <ewen.brun@icloud.com>
2018-03-10 19:03:30 +0100

    Added SplashScreen

--- __main__.py
+++ __main__.py
@@ -12,7 +12,8 @@ __email__ = "ewen.brun@ecam.fr, pierre.haon@ecam.fr"
 import sys
 from app import App
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QSplashScreen
 
 if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
     QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
@@ -22,6 +23,11 @@ if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    splashscreen = QSplashScreen(QPixmap('ui/splash.jpg'))
+    splashscreen.showMessage(
+        "Chargement de l'interface...\n\nCrée dans le cadre des TIPE 2017 - 2018\nPar Ewen BRUN et Pierre HAON\nClasses Preparatoires à l'ECAM")
+    splashscreen.show()
     window = App()
+    splashscreen.finish(window)
     window.show()
     sys.exit(app.exec_())
